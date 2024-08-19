5a1527095eefc028c90f27c5ad968703ce5bf333
Pritiranjan Rath <pritiranjan.rath@happiestminds.com>
2024-08-19 15:01:15 +0530

    'validation'

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,9 +3,11 @@ import { signOut } from "firebase/auth";
 import { auth } from "../../util/firebase";
 import { setUser } from "../../slices/authSlice";
 import { useDispatch } from "react-redux";
+import useUser from "../../hooks/useUser";
 
 const Header = () => {
   const dispatch = useDispatch();
+  const { user } = useUser();
 
   return (
     <div className="h-20 bg-blue-600 flex justify-between p-5">
@@ -15,20 +17,22 @@ const Header = () => {
         </p>
       </div>
       <div className="w-auto flex justify-around">
-        <span
-          className="text-white font-bold text-2xl cursor-pointer"
-          onClick={async () => {
-            try {
-              await signOut(auth);
-              dispatch(setUser(null));
-            } catch (err) {
-              console.log(err);
-            }
-          }}
-        >
-          <LogoutIcon />
-          Sign Out
-        </span>
+        {user && (
+          <span
+            className="text-white font-bold text-2xl cursor-pointer"
+            onClick={async () => {
+              try {
+                await signOut(auth);
+                dispatch(setUser(null));
+              } catch (err) {
+                console.log(err);
+              }
+            }}
+          >
+            <LogoutIcon />
+            Sign Out
+          </span>
+        )}
       </div>
     </div>
   );
